379da091b595b8dc0dd3eb591b9aab40e5eca4fd
Roger <roger45663@gmail.com>
2021-09-04 17:52:59 +0800

    單文件組件結構練習

---
+++ 19_單文件元件/School.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="demo">
+        <p>學校名稱: {{ name }}</p>
+        <p>學校地址: {{ address }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "School",
+    data() {
+        return {
+            name: "尚硅谷",
+            address: "上海",
+        };
+    },
+};
+</script>
+
+<style>
+.demo {
+    background-color: yellow;
+    color: green;
+}
+</style>
---
+++ 19_單文件元件/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>學生名字: {{ name }}</p>
+        <p>學生年齡: {{ age }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Student",
+    data() {
+        return {
+            name: "Peter",
+            age: 22,
+        };
+    },
+};
+</script>
